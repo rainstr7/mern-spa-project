dd39db93469aac5883c5ec7cee690772687d3c7b
rainstr7 <rainstr7@gmail.com>
2021-05-14 08:44:26 +0300

    something wrong

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const config = require('config');
 const mongoose = require('mongoose');
+
 const app = express();
 
+app.use(express.json({extended: true}));
 app.use('/api/auth', require('./routes/auth.router'));
 
 const PORT = config.get('port') || 5000;
--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, {useState}  from 'react';
+import { useHttp } from "../hooks/http.hook";
 
 export const AuthPage = () => {
+    const {loading, error, request} = useHttp();
+    const [form, setForm] = useState({
+        email: '',
+        password: '',
+    });
+     const changeHandler = (event) => {
+        setForm({ ...form, [event.target.name]: event.target.value })
+     }
+
+     const registerHandler = async () => {
+        try {
+             const data = await request('/api/auth/register', 'POST', {...form});
+             console.log("Data", data);
+        } catch (e) {
+
+        }
+     }
+
     return (
         <div className="row">
             <div className="col s6 offset-s3">
@@ -16,6 +35,7 @@ export const AuthPage = () => {
                                     type="text"
                                     name="email"
                                     className="yellow-input"
+                                    onChange={changeHandler}
                                 />
                                     <label htmlFor="email">Email</label>
                             </div>
@@ -26,14 +46,27 @@ export const AuthPage = () => {
                                     type="password"
                                     name="password"
                                     className="yellow-input"
+                                    onChange={changeHandler}
                                 />
                                 <label htmlFor="password">Пароль</label>
                             </div>
                         </div>
                     </div>
                     <div className="card-action">
-                        <button className="btn yellow darken-4" style={{marginRight: 10}}>Войти</button>
-                        <button className="btn grey lighten-1 black-text">Регистрация</button>
+                        <button
+                            className="btn yellow darken-4"
+                            style={{marginRight: 10}}
+                            disabled={loading}
+                        >
+                            Войти
+                        </button>
+                        <button
+                            className="btn grey lighten-1 black-text"
+                            onClick={registerHandler}
+                            disabled={loading}
+                        >
+                            Регистрация
+                        </button>
                     </div>
                 </div>
             </div>
